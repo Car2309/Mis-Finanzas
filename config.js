6d8749997d44f4193b7f6fc559dc709c56b102bc
Car2309 <carovigue0309@gmail.com>
2026-08-18 16:46:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // Pega aquí la URL de tu implementación de Apps Script (la que
 // termina en /exec). La consigues en: Implementar > Administrar
 // implementaciones, en tu editor de Apps Script.
-const API_URL = 'https://script.google.com/macros/s/AKfycbyYrYoTcMbJ-M2UhIU-pj7_jkRX65IFO9KKPF0h09YFHyxSn1wFYvqUSjZ2lPV57dGvAA/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxYIMyghA0M1_8Pfdcs5diduCySWN6PUaJErYPlkB_Dd9oFqen6Tf7hnqjvRPNSx9AA/exec';
 
 // La misma llave que pusiste en la variable API_KEY de Code.gs.
-const API_KEY = 'q0RUm-tKprAogLl1QRoqOQr-QfX_eSLhW24NOKoV2Bo';
+const API_KEY = 'AKfycbxYIMyghA0M1_8Pfdcs5diduCySWN6PUaJErYPlkB_Dd9oFqen6Tf7hnqjvRPNSx9AA';
