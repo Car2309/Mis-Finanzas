6320f9d021fd4e19a7d1195e7ef6215fc08980d1
Car2309 <carovigue0309@gmail.com>
2026-08-18 16:55:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,4 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbxYIMyghA0M1_8Pfdcs5diduCySWN6PUaJErYPlkB_Dd9oFqen6Tf7hnqjvRPNSx9AA/exec';
 
 // La misma llave que pusiste en la variable API_KEY de Code.gs.
-const API_KEY = 'AKfycbxYIMyghA0M1_8Pfdcs5diduCySWN6PUaJErYPlkB_Dd9oFqen6Tf7hnqjvRPNSx9AA';
+const API_KEY = 'g-yH8FGz1O7caJI4p_uA42zAUEkynlgCb99q32D9ceY';
