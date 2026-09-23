8ad95029cc8bae2c0f39f6b525cbbb813cb3f09e
Car2309 <carovigue0309@gmail.com>
2026-09-23 18:41:49 -0500

    Ajuste Margin

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // internet. Los datos (Sheets) SIEMPRE necesitan conexión; esto solo
 // evita la pantalla en blanco cuando no hay señal.
 
-const CACHE_NAME = 'control-gastos-novia-v7';
+const CACHE_NAME = 'control-gastos-novia-v8';
 const ASSETS = [
   './',
   './index.html',
