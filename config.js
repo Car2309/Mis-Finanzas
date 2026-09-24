dbe8c8155617618eafc3b3438b025684dc970cd0
Car2309 <carovigue0309@gmail.com>
2026-09-23 20:11:07 -0500

    New - BD

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // Pega aquí la URL de tu implementación de Apps Script (la que
 // termina en /exec). La consigues en: Implementar > Administrar
 // implementaciones, en tu editor de Apps Script.
-const API_URL = 'https://script.google.com/macros/s/AKfycbzD49kGuQnVi83s79B39lw4ylALwPb-ZwSDiQ9YVk1ug61zfEQpzWMymcZPvjDG7V1H/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzaTk9yW8nH5m0zo5evdkP-7h2X1Djg7YTcULWcQXHdzUEkfhceKOdrqW7wNJCMftu8Ng/exec';
 
 // La misma llave que pusiste en la variable API_KEY de Code.gs.
 const API_KEY = 'g-yH8FGz1O7caJI4p_uA42zAUEkynlgCb99q32D9ceY';
